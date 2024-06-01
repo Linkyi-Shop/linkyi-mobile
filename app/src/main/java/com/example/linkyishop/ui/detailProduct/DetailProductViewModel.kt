3c39dec756d9f05949656e822be95010757769c6
Kinn-01 <gilmanarief23@gmail.com>
2024-06-01 16:08:23 +0700

    new branch

---
+++ app/src/main/java/com/example/linkyishop/ui/detailProduct/DetailProductViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.linkyishop.ui.detailProduct
+
+import androidx.lifecycle.ViewModel
+import com.example.linkyishop.data.repository.UserRepository
+
+class DetailProductViewModel(private val repository: UserRepository): ViewModel() {
+}
